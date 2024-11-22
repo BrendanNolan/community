de877ad56469f06776c5bb4016ff3dc351287c24
Brendan Nolan <bnolan23@gmail.com>
2024-11-22 13:59:59 +0000

    Got let statements working with the toy name of sturgeon

--- lang/rust/brendan_rust.py
+++ lang/rust/brendan_rust.py
@@ -1,7 +1,33 @@
-from talon import actions
+from talon import Context, Module, actions
 
-def sturgeon(text: str):
-    actions.auto_insert("let "+ to_snake_case(text) + " = ")
-    actions.key("escape")
-    actions.key("A")
+mod = Module()
+
+@mod.action_class
+class Actions:
+
+    def sturgeon(text: str):
+        "Inserts a let statement"
+
+
+ctx = Context()
+ctx.matches = r"""
+code.language: rust
+"""
+
+@ctx.action_class("user")
+class UserActions:
+
+    def sturgeon(text: str):
+        actions.auto_insert("let "+ to_snake_case(text) + " = ")
+        actions.key("escape")
+        actions.key("A")
+
+
+def to_snake_case(text):
+    import re
+    # Replace spaces or hyphens with underscores
+    text = re.sub(r"[\s\-]+", "_", text)
+    # Add underscores before uppercase letters, then lower the entire string
+    text = re.sub(r"([a-z])([A-Z])", r"\1_\2", text)
+    return text.lower()
 
