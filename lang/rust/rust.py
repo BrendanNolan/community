752abe5b4e478de14dce3c63a70fa60a5ac74251
Brendan Nolan <bnolan23@gmail.com>
2024-12-02 13:51:42 +0000

    Revert "Making my own rust commands"
    This reverts commit 0d354adbe3ebe8fd3b70916c749304dd1f90b870.

--- lang/rust/rust.py
+++ lang/rust/rust.py
@@ -1,41 +1,39 @@
-from talon import Context, Module, actions
-import re
+from typing import Any, Callable, TypeVar
+
+from talon import Context, Module, actions, settings
 
 mod = Module()
+# rust specific grammar
+mod.list("code_type_modifier", desc="List of type modifiers for active language")
+mod.list("code_macros", desc="List of macros for active language")
+mod.list("code_trait", desc="List of traits for active language")
 
 
 @mod.action_class
 class Actions:
+    def code_state_implements():
+        """Inserts implements block, positioning the cursor appropriately"""
 
-    def param(text: str):
-        "Inserts a param"
+    def code_insert_macro(text: str, selection: str):
+        """Inserts a macro and positions the cursor appropriately"""
 
-    def let(text: str):
-        "Inserts a let statement"
+    def code_insert_macro_array(text: str, selection: str):
+        """Inserts a macro array and positions the cursor appropriately"""
 
-    def let_mut(text: str):
-        "Inserts a let mut statement"
+    def code_insert_macro_block(text: str, selection: str):
+        """Inserts a macro block and positions the cursor appropriately"""
 
-    def method_call(text: str):
-        "Calls method"
+    def code_state_unsafe():
+        """Inserts an unsafe block and positions the cursor appropriately"""
 
-    def loop():
-        "Loop"
+    def code_comment_documentation_block():
+        """Inserts a block document comment and positions the cursor appropriately"""
 
-    def while_loop():
-        "For loop"
+    def code_comment_documentation_inner():
+        """Inserts an inner document comment and positions the cursor appropriately"""
 
-    def for_loop():
-        "For loop"
-
-    def mod_block(text: str):
-        "Module block"
-
-    def func(text: str):
-        "Function"
-
-    def pub_func(text: str):
-        "Function"
+    def code_comment_documentation_block_inner():
+        """Inserts an inner block document comment and positions the cursor appropriately"""
 
 
 ctx = Context()
@@ -43,94 +41,476 @@ ctx.matches = r"""
 code.language: rust
 """
 
+scalar_types = {
+    "eye eight": "i8",
+    "you eight": "u8",
+    "bytes": "u8",
+    "eye sixteen": "i16",
+    "you sixteen": "u16",
+    "eye thirty two": "i32",
+    "you thirty two": "u32",
+    "eye sixty four": "i64",
+    "you sixty four": "u64",
+    "eye one hundred and twenty eight": "i128",
+    "you one hundred and twenty eight": "u128",
+    "eye size": "isize",
+    "you size": "usize",
+    "float thirty two": "f32",
+    "float sixty four": "f64",
+    "boolean": "bool",
+    "character": "char",
+}
+
+compound_types = {
+    "tuple": "()",
+    "array": "[]",
+}
+
+standard_library_types = {
+    "box": "Box",
+    "vector": "Vec",
+    "string": "String",
+    "string slice": "&str",
+    "os string": "OsString",
+    "os string slice": "&OsStr",
+    "see string": "CString",
+    "see string slice": "&CStr",
+    "option": "Option",
+    "result": "Result",
+    "hashmap": "HashMap",
+    "hash set": "HashSet",
+    "reference count": "Rc",
+}
+
+standard_sync_types = {
+    "arc": "Arc",
+    "barrier": "Barrier",
+    "condition variable": "Condvar",
+    "mutex": "Mutex",
+    "once": "Once",
+    "read write lock": "RwLock",
+    "receiver": "Receiver",
+    "sender": "Sender",
+    "sink sender": "SyncSender",
+}
+
+all_types = {
+    **scalar_types,
+    **compound_types,
+    **standard_library_types,
+    **standard_sync_types,
+}
+
+standard_function_macros = {
+    "panic": "panic!",
+    "format": "format!",
+    "concatenate": "concat!",
+    "print": "print!",
+    "print line": "println!",
+    "error print line": "eprintln!",
+    "to do": "todo!",
+}
+
+standard_array_macros = {
+    "vector": "vec!",
+}
+
+standard_block_macros = {
+    "macro rules": "macro_rules!",
+}
+
+logging_macros = {
+    "debug": "debug!",
+    "info": "info!",
+    "warning": "warn!",
+    "error": "error!",
+}
+
+testing_macros = {
+    "assert": "assert!",
+    "assert equal": "assert_eq!",
+    "assert not equal": "assert_ne!",
+}
+
+all_function_macros = {
+    **standard_function_macros,
+    **logging_macros,
+    **testing_macros,
+}
+
+all_array_macros = {
+    **standard_array_macros,
+}
+
+all_block_macros = {
+    **standard_block_macros,
+}
+
+all_macros = {
+    **all_function_macros,
+    **all_array_macros,
+    **all_block_macros,
+}
+
+all_function_macro_values = set(all_function_macros.values())
+all_array_macro_values = set(all_array_macros.values())
+all_block_macro_values = set(all_block_macros.values())
+
+closure_traits = {
+    "closure": "Fn",
+    "closure once": "FnOnce",
+    "closure mutable": "FnMut",
+}
+
+conversion_traits = {
+    "into": "Into",
+    "from": "From",
+}
+
+iterator_traits = {
+    "iterator": "Iterator",
+}
+
+all_traits = {
+    **closure_traits,
+    **conversion_traits,
+    **iterator_traits,
+}
+
+
+# tag: libraries_gui
+ctx.lists["user.code_libraries"] = {
+    "eye oh": "std::io",
+    "file system": "std::fs",
+    "envy": "std::env",
+    "collections": "std::collections",
+}
+
+# tag: functions_common
+ctx.lists["user.code_common_function"] = {
+    "drop": "drop",
+    "catch unwind": "catch_unwind",
+    "iterator": "iter",
+    "into iterator": "into_iter",
+    "from iterator": "from_iter",
+    **all_macros,
+}
+
+# tag: functions
+ctx.lists["user.code_type"] = all_types
+
+# rust specific grammar
+ctx.lists["user.code_type_modifier"] = {
+    "mutable": "mut ",
+    "mute": "mut ",
+    "borrowed": "&",
+    "borrowed mutable": "&mut ",
+    "borrowed mute": "&mut ",
+    "mutable borrowed": "&mut ",
+    "mute borrowed": "&mut ",
+}
+
+
+@ctx.capture("user.code_type", rule="[{user.code_type_modifier}] {user.code_type}")
+def code_type(m) -> str:
+    """Returns a macro name"""
+    return "".join(m)
+
+
+ctx.lists["user.code_macros"] = all_macros
+
+ctx.lists["user.code_trait"] = all_traits
+
 
 @ctx.action_class("user")
 class UserActions:
+    # tag: comment_line
 
-    def param(text: str):
-        actions.insert(to_snake_case(text) + ": ")
+    def code_comment_line_prefix():
+        actions.auto_insert("// ")
 
-    def let(text: str):
-        actions.insert("let " + to_snake_case(text) + " = ")
+    # tag: comment_documentation
 
-    def let_mut(text: str):
-        actions.insert("let mut " + to_snake_case(text) + " = ")
+    def code_comment_documentation():
+        actions.auto_insert("/// ")
+
+    # tag: imperative
+
+    def code_state_if():
+        actions.auto_insert("if ")
+
+    def code_state_else_if():
+        actions.auto_insert(" else if ")
+
+    def code_state_else():
+        actions.user.insert_between(" else { ", " }")
+
+    def code_state_switch():
+        actions.auto_insert("match ")
+
+    def code_state_for():
+        actions.auto_insert("for  in  {}")
+        actions.edit.left()
+        actions.key("enter")
+        actions.edit.up()
+        actions.edit.line_end()
+        repeat_call(6, actions.edit.left)
+
+    def code_state_while():
+        actions.auto_insert("while  {}")
+        actions.edit.left()
+        actions.key("enter")
+        actions.edit.up()
+        actions.edit.line_end()
+        repeat_call(2, actions.edit.left)
+
+    def code_state_infinite_loop():
+        actions.user.insert_between("loop {", "}")
+        actions.key("enter")
+
+    def code_state_return():
+        actions.auto_insert("return ")
+
+    def code_break():
+        actions.auto_insert("break;")
+
+    def code_next():
+        actions.auto_insert("continue;")
+
+    # tag: object_oriented
+
+    def code_operator_object_accessor():
+        actions.auto_insert(".")
+
+    def code_self():
+        actions.auto_insert("self")
+
+    def code_define_class():
+        actions.auto_insert("struct ")
+
+    # tag: data_bool
+
+    def code_insert_true():
+        actions.auto_insert("true")
+
+    def code_insert_false():
+        actions.auto_insert("false")
+
+    # tag: data_null
+
+    def code_insert_null():
+        actions.auto_insert("None")
+
+    def code_insert_is_null():
+        actions.auto_insert(".is_none()")
+
+    def code_insert_is_not_null():
+        actions.auto_insert(".is_some()")
+
+    # tag: functions
+
+    def code_default_function(text: str):
+        actions.user.code_private_function(text)
+
+    def code_private_function(text: str):
+        actions.auto_insert("fn ")
+        formatter = settings.get("user.code_private_function_formatter")
+        function_name = actions.user.formatted_text(text, formatter)
+        actions.user.code_insert_function(function_name, None)
+
+    def code_protected_function(text: str):
+        actions.auto_insert("pub(crate) fn ")
+        formatter = settings.get("user.code_protected_function_formatter")
+        function_name = actions.user.formatted_text(text, formatter)
+        actions.user.code_insert_function(function_name, None)
+
+    def code_public_function(text: str):
+        actions.auto_insert("pub fn ")
+        formatter = settings.get("user.code_public_function_formatter")
+        function_name = actions.user.formatted_text(text, formatter)
+        actions.user.code_insert_function(function_name, None)
+
+    def code_insert_type_annotation(type: str):
+        actions.auto_insert(f": {type}")
+
+    def code_insert_return_type(type: str):
+        actions.auto_insert(f" -> {type}")
+
+    # tag: functions_gui
+
+    def code_insert_function(text: str, selection: str):
+        code_insert_function_or_macro(text, selection, "(", ")")
+
+    # tag: libraries
+
+    def code_import():
+        actions.auto_insert("use ")
+
+    # tag: libraries_gui
+
+    def code_insert_library(text: str, selection: str):
+        actions.user.paste(f"use {text}")
+
+    # tag: operators_array
+
+    def code_operator_subscript():
+        actions.auto_insert("[]")
+        actions.edit.left()
+
+    # tag: code_operators_assignment
+
+    def code_operator_assignment():
+        actions.auto_insert(" = ")
+
+    def code_operator_subtraction_assignment():
+        actions.auto_insert(" -= ")
+
+    def code_operator_addition_assignment():
+        actions.auto_insert(" += ")
+
+    def code_operator_multiplication_assignment():
+        actions.auto_insert(" *= ")
+
+    def code_operator_division_assignment():
+        actions.auto_insert(" /= ")
+
+    def code_operator_modulo_assignment():
+        actions.auto_insert(" %= ")
+
+    def code_operator_bitwise_and_assignment():
+        actions.auto_insert(" &= ")
+
+    def code_operator_bitwise_or_assignment():
+        actions.auto_insert(" |= ")
+
+    def code_operator_bitwise_exclusive_or_assignment():
+        actions.auto_insert(" ^= ")
+
+    def code_operator_bitwise_left_shift_assignment():
+        actions.auto_insert(" <<= ")
+
+    def code_operator_bitwise_right_shift_assignment():
+        actions.auto_insert(" >>= ")
+
+    # tag: operators_bitwise
+
+    def code_operator_bitwise_and():
+        actions.auto_insert(" & ")
+
+    def code_operator_bitwise_or():
+        actions.auto_insert(" | ")
+
+    def code_operator_bitwise_exclusive_or():
+        actions.auto_insert(" ^ ")
+
+    def code_operator_bitwise_left_shift():
+        actions.auto_insert(" << ")
+
+    def code_operator_bitwise_right_shift():
+        actions.auto_insert(" >> ")
+
+    # tag: operators_math
+
+    def code_operator_subtraction():
+        actions.auto_insert(" - ")
+
+    def code_operator_addition():
+        actions.auto_insert(" + ")
+
+    def code_operator_multiplication():
+        actions.auto_insert(" * ")
+
+    def code_operator_exponent():
+        actions.auto_insert(".pow()")
+        actions.edit.left()
+
+    def code_operator_division():
+        actions.auto_insert(" / ")
+
+    def code_operator_modulo():
+        actions.auto_insert(" % ")
+
+    def code_operator_equal():
+        actions.auto_insert(" == ")
+
+    def code_operator_not_equal():
+        actions.auto_insert(" != ")
+
+    def code_operator_greater_than():
+        actions.auto_insert(" > ")
+
+    def code_operator_greater_than_or_equal_to():
+        actions.auto_insert(" >= ")
+
+    def code_operator_less_than():
+        actions.auto_insert(" < ")
+
+    def code_operator_less_than_or_equal_to():
+        actions.auto_insert(" <= ")
+
+    def code_operator_and():
+        actions.auto_insert(" && ")
+
+    def code_operator_or():
+        actions.auto_insert(" || ")
+
+    def code_operator_increment():
+        actions.auto_insert(" += 1")
+
+    # rust specific grammar
+
+    def code_operator_structure_dereference():
+        actions.auto_insert("*")
+
+    def code_state_implements():
+        actions.auto_insert("impl  {}")
+        actions.edit.left()
+        actions.key("enter")
+        actions.edit.up()
+        actions.edit.line_end()
+        repeat_call(2, actions.edit.left)
+
+    def code_insert_macro(text: str, selection: str):
+        if text in all_array_macro_values:
+            code_insert_function_or_macro(text, selection, "[", "]")
+        elif text in all_block_macro_values:
+            code_insert_function_or_macro(text, selection, "{", "}")
+        else:
+            code_insert_function_or_macro(text, selection, "(", ")")
+
+    def code_state_unsafe():
+        actions.user.insert_between("unsafe {", "}")
+        actions.key("enter")
+
+    def code_comment_documentation_block():
+        actions.user.insert_between("/**", "*/")
+        actions.key("enter")
+
+    def code_comment_documentation_inner():
+        actions.auto_insert("//! ")
+
+    def code_comment_documentation_block_inner():
+        actions.user.insert_between("/*!", "*/")
+        actions.key("enter")
 
 
-    def method_call(text: str):
-        l, r = prepare_for_method_call(text)
-        actions.insert(to_snake_case(l) + "." + to_snake_case(r) + "()")
-        esc("i")
-
-    def loop():
-        actions.insert("loop {")
-        enter()
-
-    def while_loop():
-        actions.insert("while  {")
-        enter()
-        escape()
-        actions.insert("?while")
-        enter()
-        actions.insert("Ela")
-
-    def for_loop():
-        actions.insert("for  {")
-        enter()
-        escape()
-        actions.insert("?for")
-        enter()
-        actions.insert("Ela")
-
-    def mod_block(text: str):
-        actions.insert("mod " + to_snake_case(text) + " {")
-        enter()
-
-    def func(text: str):
-        actions.insert("fn " + to_snake_case(text) + "() {\n")
-        escape()
-        actions.insert("?(")
-        enter()
-        actions.insert("a")
-
-    def pub_func(text: str):
-        actions.insert("pub fn " + to_snake_case(text) + "() {\n")
-        escape()
-        actions.insert("?(")
-        enter()
-        actions.insert("a")
+def code_insert_function_or_macro(
+    text: str,
+    selection: str,
+    left_delim: str,
+    right_delim: str,
+):
+    if selection:
+        out_text = text + f"{left_delim}{selection}{right_delim}"
+    else:
+        out_text = text + f"{left_delim}{right_delim}"
+    actions.user.paste(out_text)
+    actions.edit.left()
 
 
-def escape():
-    actions.key("escape")
+RT = TypeVar("RT")  # return type
 
 
-def enter():
-    actions.key("enter")
-
-
-def esc(k):
-    actions.key("escape")
-    actions.key(k)
-
-
-def to_snake_case(text):
-    # Replace spaces or hyphens with underscores
-    text = re.sub(r"[\s\-]+", "_", text)
-    # Add underscores before uppercase letters, then lower the entire string
-    text = re.sub(r"([a-z])([A-Z])", r"\1_\2", text)
-    return text.lower()
-
-
-def to_upper_camel(text):
-    return ''.join(word.capitalize() for word in re.split(r'[ _-]+', text))
-
-
-def prepare_for_method_call(word: str):
-    """Splits word on call"""
-    parts = word.split("invoke", 1)
-    left = parts[0].strip() if parts else ""
-    right = parts[1].strip() if len(parts) > 1 else ""
-    if not right:
-        return "", left
-    return left, right
+def repeat_call(n: int, f: Callable[..., RT], *args: Any, **kwargs: Any):
+    for i in range(n):
+        f(*args, **kwargs)
--- lang/rust/rust.talon
+++ lang/rust/rust.talon
@@ -1,40 +1,106 @@
 code.language: rust
 -
-constant <user.text>$: user.let(text)
-variable <user.text>$: user.let_mut(text)
-method <user.text>$: user.method_call(text)
-(mod | module) <user.text>$: user.mod_block(text)
+tag(): user.code_comment_line
+tag(): user.code_comment_block_c_like
+tag(): user.code_comment_documentation
 
-(funk) <user.text>$: user.func(text)
-(pub funk) <user.text>$: user.pub_func(text)
+tag(): user.code_block_c_like
+tag(): user.code_imperative
+tag(): user.code_object_oriented
 
-param <user.text>$: user.param(text)
-add type:
-    key(escape)
-    insert("a: ")
+tag(): user.code_data_bool
+tag(): user.code_data_null
 
-# Control Flow
-loop: user.loop()
-for loop: user.for_loop()
-while loop: user.while_loop()
+tag(): user.code_functions
+tag(): user.code_functions_common
+tag(): user.code_libraries
+tag(): user.code_libraries_gui
 
-# Types
-integer eight: insert("i8")
-integer sixteen: insert("i16")
-integer thirty two: insert("i32")
-integer sixty four: insert("i64")
-float eight: insert("f8")
-float sixteen: insert("f16")
-float thirty two: insert("f32")
-float sixty four: insert("f64")
-condition variable: insert("CondVar")
-option:
-    insert("Option<>")
-    key(escape)
-    key(i)
-vec:
-    insert("Vec<>")
-    key(escape)
-    key(i)
-stir:
-    insert("str")
+tag(): user.code_operators_array
+tag(): user.code_operators_assignment
+tag(): user.code_operators_bitwise
+tag(): user.code_operators_math
+
+settings():
+    user.code_private_function_formatter = "SNAKE_CASE"
+    user.code_protected_function_formatter = "SNAKE_CASE"
+    user.code_public_function_formatter = "SNAKE_CASE"
+    user.code_private_variable_formatter = "SNAKE_CASE"
+    user.code_protected_variable_formatter = "SNAKE_CASE"
+    user.code_public_variable_formatter = "SNAKE_CASE"
+
+# rust-specific grammars
+
+## for unsafe rust
+state unsafe: "unsafe "
+unsafe block: user.code_state_unsafe()
+
+## rust centric struct and enum definitions
+state (struct | structure) <user.text>:
+    insert("struct ")
+    insert(user.formatted_text(text, "PUBLIC_CAMEL_CASE"))
+
+state enum <user.text>:
+    insert("enum ")
+    insert(user.formatted_text(text, "PUBLIC_CAMEL_CASE"))
+
+toggle use: user.code_toggle_libraries()
+
+## Simple aliases
+borrow: "&"
+borrow mutable: "&mut "
+state (a sink | async | asynchronous): "async "
+state (pub | public): "pub "
+state (pub | public) crate: "pub(crate) "
+state (dyn | dynamic): "dyn "
+state constant: "const "
+state (funk | func | function): "fn "
+state (imp | implements): "impl "
+state let mute: "let mut "
+state let: "let "
+state (mute | mutable): "mut "
+state (mod | module): "mod "
+state ref (mute | mutable): "ref mut "
+state ref: "ref "
+state trait: "trait "
+state match: user.code_state_switch()
+state (some | sum): "Some"
+state static: "static "
+self taught: "self."
+state use: user.code_import()
+
+use <user.code_libraries>:
+    user.code_insert_library(code_libraries, "")
+    key(; enter)
+
+## specialist flow control
+state if let some: user.insert_between("if let Some(", ")")
+state if let (ok | okay): user.insert_between("if let Ok(", ")")
+state if let error: user.insert_between("if let Err(", ")")
+
+## rust centric synonyms
+is some: user.code_insert_is_not_null()
+
+## for implementing
+implement (struct | structure): user.code_state_implements()
+
+## for annotating function parameters
+is implemented trait {user.code_trait}: ": impl {code_trait}"
+is implemented trait: ": impl "
+returns implemented trait {user.code_trait}: " -> impl {code_trait}"
+returns implemented trait: " -> impl "
+
+## for generic reference of traits
+trait {user.code_trait}: insert("{code_trait}")
+implemented trait {user.code_trait}: insert("impl {code_trait}")
+dynamic trait {user.code_trait}: insert("dyn {code_trait}")
+
+## for generic reference of macro
+macro {user.code_macros}: user.code_insert_macro(code_macros, "")
+macro wrap {user.code_macros}:
+    user.code_insert_macro(code_macros, edit.selected_text())
+
+## rust specific document comments
+block dock comment: user.code_comment_documentation_block()
+inner dock comment: user.code_comment_documentation_inner()
+inner block dock comment: user.code_comment_documentation_block_inner()
