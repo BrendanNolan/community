69506c66c29ea9bc37e63f59045e52b86a45ed74
Brendan Nolan <bnolan23@gmail.com>
2024-12-04 08:30:27 +0000

    Make sure the word 'leap' works for leap.nvim

--- core/text/formatters.py
+++ core/text/formatters.py
@@ -282,7 +282,7 @@ word_formatter_names = {
     "word": "ALL_LOWERCASE",
     "trot": "TRAILING_SPACE,ALL_LOWERCASE",
     "proud": "CAPITALIZE_FIRST_WORD",
-    "leap": "TRAILING_SPACE,CAPITALIZE_FIRST_WORD",
+    "leaping": "TRAILING_SPACE,CAPITALIZE_FIRST_WORD",
 }
 
 
